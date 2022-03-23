abd4437ccdd5263a72b6433128041c49759676bf
arken <96731415+arkenfx@users.noreply.github.com>
2022-03-23 13:36:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -37,16 +37,16 @@ const start = async () => {
         const chatId = msg.chat.id;
 
         try {
-            if (text === "/start") {
+            if (text === '/start') {
                 await UserModel.create({chatId})
                 await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
                 return bot.sendMessage(chatId, `Добро пожаловать в телеграм бот автора ютуб канала ULBI TV`);
             }
-            if (text === "/info") {
+            if (text === '/info') {
                 const user = await UserModel.findOne({chatId})
                 return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}, в игре у тебя правильных ответов ${user.right}, неправильных ${user.wrong}`);
             }
-            if (text === "/game") {
+            if (text === '/game') {
                 return startGame(chatId);
             }
             return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!)');
@@ -59,7 +59,7 @@ const start = async () => {
     bot.on('callback_query', async msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
-        if (data === "/again") {
+        if (data === '/again') {
             return startGame(chatId)
         }
         const user = await UserModel.findOne({chatId})
